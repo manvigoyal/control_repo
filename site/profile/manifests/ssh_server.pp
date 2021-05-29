32e9ba36455f047438a9aee68c2991c31887fb91
Manvi Goyal <79033458+manvigoyal@users.noreply.github.com>
2021-05-29 14:21:51 +0530

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDml3cyFTmBSh56XHyf+8HFRVlQ44ZfqPMP2SJn71TR0tpNDxjQse+yEe1egzT/4NX/qfMBDUpvwJURY2UvEyef0uTm3BmKwHigS8lY0BCJYq14smj5TL5f82T0L2Gd5XwjwQAs70Z6Q30tzx7a41J814Z2VPgS2iUmzfScPz8b4f1dMlsRxRMVB3uj6iijiuSv2vP1tIxXoh8FbVrtW19jneA+fDozm2mPCMThFJQ8J/dJeAZ3uWCQ+DQZD9LDl2qGKvu5Bxszp79N5dHhm115nX61oj8Tw253tSBcIUoRu0szuFbKmk2iM8rYQdK24hp9va8BCjykcrEU7rE1o9j1',
   }  
 }
